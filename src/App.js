25623be18674785baacf587d67f5c27f809130e0
Jacob Garcia <jacobroxasgarcia@yahoo.com>
2022-07-07 16:29:35 -0700

    Issues with DataContext not being able to be imported properly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
-
+import DataContext  from './context/DataContext'
 
 function App() {
   let [search, setSearch] = useState('')
@@ -32,10 +32,12 @@ function App() {
   }
 
   return (
-      <div>
+      <div className ="App">
           <SearchBar handleSearch={handleSearch} />
           {message}
-          <Gallery data={data} />
+          <DataContext.Provider value = {data} >
+            <Gallery/>
+          </DataContext.Provider>
       </div>
   )
 }
