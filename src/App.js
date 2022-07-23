f5b9bd3e958b59cce3ff906029df128650b3ad64
Jacob Garcia <jacobroxasgarcia@yahoo.com>
2022-07-23 13:10:07 -0700

    commit -m" with suspense"

--- src/App.js
+++ src/App.js
@@ -1,37 +1,49 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, Suspense } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 import AlbumView from './components/AlbumView'
 import ArtistView from './components/ArtistView'
+import { createResource as fetchData } from './helper'
 
 function App() {
     let [search, setSearch] = useState('')
     let [message, setMessage] = useState('Search for Music!')
-    let [data, setData] = useState([])
+    let [data, setData] = useState(null)
 
     const API_URL = 'https://itunes.apple.com/search?term='
 
     useEffect(() => {
-        if(search) {
-            const fetchData = async () => {
-                document.title = `${search} Music`
-                const response = await fetch(API_URL + search)
-                const resData = await response.json()
-                if (resData.results.length > 0) {
-                    return setData(resData.results)
-                } else {
-                    return setMessage('Not Found')
-                }
-            }
-            fetchData()
+        if (searchTerm) {
+            setData(fetchData(searchTerm))
         }
-    }, [search])
+    }, [searchTerm])
+    
     
     const handleSearch = (e, term) => {
         e.preventDefault()
         setSearch(term)
     }
+
+    // return (
+    //     <div className="App">
+    //         <SearchBar handleSearch={handleSearch} />
+    //         {message}
+    //         <Suspense fallback={<h1>Loading...</h1>}>
+    //             // <Gallery data={data} />
+    //         </Suspense>
+    //     </div>
+    // )
+
+    // return (
+    //     <div className="App">
+    //         <SearchBar handleSearch={handleSearch} />
+    //         {message}
+    //         {renderGallery()}
+    //     </div>
+    // )
+    
+    
 
     return (
         <div>
@@ -50,6 +62,15 @@ function App() {
             </Router>
         </div>
     )    
+    const renderGallery = () => {
+        if(data) {
+            return (
+                <Suspense fallback={<Spinner />}>
+                    <Gallery data={data} />
+                </Suspense>
+            )
+        }
+    }       
 }
 
 export default App
